3536908ba3d52bef710a5119b2e05cbc6e22a324
SyarifMuhammadIhsanRismawan-12210770 <12210770@bsi.ac.id>
2022-11-30 17:16:24 +0700

    Migration, Models, Seeder, Test email

---
+++ app/Database/Seeds/KamartarifSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use App\Models\KamartarifModel;
+use CodeIgniter\Database\Seeder;
+
+class KamartarifSeeder extends Seeder
+{
+    public function run()
+    {
+        $r = (new KamartarifModel())->insert([
+            'kamartipe_id' => 1,
+            'tarif' => 2000000,
+            'tgl_mulai' => '2022-11-02',
+            'tgl_selesai' => '2022-11-05',
+            'tipetarif_id' => 1,
+        ]);
+        echo "hasil insert $r";
+    }
+}
